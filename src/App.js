135e318745d97f461a0b767ff3bea0e9c388eb5b
Jaosn Nuhn <jasonnuhn@Jaosns-MacBook-Air.local>
2017-10-25 09:35:16 -0400

    progress commit

--- src/App.js
+++ src/App.js
@@ -8,11 +8,15 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Surdi</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Connect with the World, one sign at a time
         </p>
+          <h1>Where you can</h1>
+            <ul>Learn</ul>
+            <ul>Educate</ul>
+            <ul>or Connect</ul>
       </div>
     );
   }
